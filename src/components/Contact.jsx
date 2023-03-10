adfeb553d4474e3e72328e3679ff20d0bd74bcf4
Abdullah Ajayi <hafizajayi2308@gmail.com>
2023-03-10 14:50:50 +0100

    Updated Contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,18 @@
+import useMedia from "../hooks/mediaQuery";
+import { FiArrowRight } from "react-icons/fi";
 import { globalStyles } from "../styles/globalStyles";
 
 const styles = {
   input: "flex-1 w-full h-10 p-5 outline-none bg-blue-100 rounded-md",
+  btn: "px-12 w-fit p-3 mt-8 text-lg flex items-center justify-center gap-1",
 };
 
 const Contact = () => {
+  const { matchesWidth } = useMedia();
   return (
     <section
       id="contact"
-      className={`${globalStyles.section} flex flex-col md:items-center text-center`}
+      className={`${globalStyles.section} flex flex-col items-center text-center`}
     >
       <h1 className={`animate-on-scroll ${globalStyles.heading}`}>
         Get In Touch.
@@ -22,14 +26,18 @@ const Contact = () => {
         Send me a mail.
       </p>
       <form className="box animate-on-scroll mt-10 w-full max-w-[500px] flex flex-col gap-5">
-        <div className="flex gap-3">
-          <input className={styles.input} type="text" placeholder="John Doe" />
+        <div className="flex flex-col md:flex-row  gap-3">
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Enter your name"
+          />
           <input
             className={styles.input}
             type="email"
             name="email"
             id="email"
-            placeholder="example@email.com"
+            placeholder="Enter your email"
             required
           />
         </div>
@@ -38,15 +46,18 @@ const Contact = () => {
             required
             className={`${styles.input} h-[150px]`}
             type="text"
-            placeholder="Hi, seems we need a product lead designer to join our team at company XYZ"
+            placeholder="Your message"
           />
         </div>
-        <div>
-          <input
-            className={`${globalStyles.btn} px-10 w-fit p-3 mt-8 text-lg`}
-            type="button"
-            value="Shoot"
-          />
+        <div className="flex justify-center cursor-pointer">
+          <button className={`btn ${globalStyles.btn} ${styles.btn}`}>
+            <input
+              className="text-sm font-semibold uppercase"
+              type="button"
+              value="Shoot"
+            />
+            <FiArrowRight className="arrow text-xl hover:translate-x-1" />
+          </button>
         </div>
       </form>
     </section>
