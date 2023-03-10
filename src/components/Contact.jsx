44fbbfbfce266e493ab26ed03ae566ac918a83ef
Abdullah Ajayi <hafizajayi2308@gmail.com>
2023-03-10 09:24:58 +0100

    Added form to contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,22 +1,14 @@
-import { FiGithub, FiTwitter, FiLinkedin, FiInstagram } from "react-icons/fi";
-
 import { globalStyles } from "../styles/globalStyles";
 
-const socials = [
-  { icon: <FiGithub />, link: "https://github.com/AbdullahAjayi" },
-  { icon: <FiTwitter />, link: "https://twitter.com/abdullahajayi" },
-  { icon: <FiInstagram />, link: "https://instagram.com/abdullahajayi" },
-  {
-    icon: <FiLinkedin />,
-    link: "https://www.linkedin.com/in/abdullah-ajayi-018a36239/",
-  },
-];
+const styles = {
+  input: "flex-1 w-full h-10 p-5 outline-none bg-blue-100 rounded-md",
+};
 
 const Contact = () => {
   return (
     <section
       id="contact"
-      className={`${globalStyles.section} flex flex-col md:items-center`}
+      className={`${globalStyles.section} flex flex-col md:items-center text-center`}
     >
       <h1 className={`animate-on-scroll ${globalStyles.heading}`}>
         Get In Touch.
@@ -26,32 +18,37 @@ const Contact = () => {
       >
         I am looking forward to working with a team of creative designers and
         developers working towards bringing an idea to life. If you think we
-        might be a good fit for each other, or just want to say hello, go Ahead!
-        Send me a mail{" "}
-        <a
-          href="mailto:hafizajayi2308@gmail.com"
-          className={globalStyles.secondaryBtn}
-        >
-          @hafizajayi2308@gmail.com
-        </a>
-        .
+        might be a good fit for each other, or just want to say hello, go ahead!
+        Send me a mail.
       </p>
-      <div className="md:hidden mt-5">
-        <p className="animate-on-scroll text-lg italic">
-          Or connect with me on Social media
-        </p>
-        <ul className="flex md:hidden gap-6 text-xl mt-5">
-          {socials.map((item, index) => (
-            <li key={index} className="animate-on-scroll">
-              <a href={item.link} target="_blank" rel="noreferrer">
-                <div className="cursor-pointer hover:text-blue-700">
-                  {item.icon}
-                </div>
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <form className="box animate-on-scroll mt-10 w-full max-w-[500px] flex flex-col gap-5">
+        <div className="flex gap-3">
+          <input className={styles.input} type="text" placeholder="John Doe" />
+          <input
+            className={styles.input}
+            type="email"
+            name="email"
+            id="email"
+            placeholder="example@email.com"
+            required
+          />
+        </div>
+        <div className="w-full">
+          <input
+            required
+            className={`${styles.input} h-[150px]`}
+            type="text"
+            placeholder="Hi, seems we need a product lead designer to join our team at company XYZ"
+          />
+        </div>
+        <div>
+          <input
+            className={`${globalStyles.btn} px-10 w-fit p-3 mt-8 text-lg`}
+            type="button"
+            value="Shoot"
+          />
+        </div>
+      </form>
     </section>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,26 @@
+import { socials } from "../data/dummy";
 import { globalStyles } from "../styles/globalStyles";
 
 const Footer = () => {
   return (
-    <div className={`${globalStyles.section} pb-10`}>
+    <div className={`${globalStyles.section} pb-5`}>
       <div className="border-t border-[#090e50] pt-5 sm:text-center">
+        <ul className="flex justify-center md:hidden gap-10 pb-4 text-xl mt-5">
+          {socials.map((item, index) => (
+            <li key={index} className="">
+              <a href={item.link} target="_blank" rel="noreferrer">
+                <div className="cursor-pointer hover:text-blue-700">
+                  {item.icon}
+                </div>
+              </a>
+            </li>
+          ))}
+        </ul>
         <a
           href="https://github.com/AbdullahAjayi"
           target="_blank"
           rel="noreferrer"
-          className="animate-on-scroll text-sm text-blue-700 underline hover:no-underline"
+          className="text-sm text-blue-700 underline hover:no-underline"
         >
           Designed & built by Abdullah Ajayi
         </a>
