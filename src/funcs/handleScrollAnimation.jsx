cf24ff7973144b8ebcd5cdd487787bcdd123c767
Abdullah Ajayi <hafizajayi2308@gmail.com>
2023-03-10 21:48:04 +0100

    Updated handleScrollAnimation function

--- src/funcs/handleScrollAnimation.jsx
+++ src/funcs/handleScrollAnimation.jsx
@@ -5,21 +5,13 @@ const handleScrollAnimation = () => {
     scrollAnimElements.forEach((element) => {
       if (element.classList.contains("box")) {
         if (elementInView(element, 1 / 2)) {
-          element.style.opacity = "1";
-          element.style.transition = "opacity .5s .3s";
           element.classList.add("slide-top");
         } else {
-          element.style.opacity = "0";
-          element.style.transition = "opacity .3s";
           element.classList.remove("slide-top");
         }
       } else if (elementInView(element, 1 / 4)) {
-        element.style.opacity = "1";
-        element.style.transition = "opacity .5s .3s";
         element.classList.add("slide-top");
       } else {
-        element.style.opacity = "0";
-        element.style.transition = "opacity .3s";
         element.classList.remove("slide-top");
       }
     });
