e51e8f0dd83c4607c408bec53d933071e56b8292
Abdullah Ajayi <hafizajayi2308@gmail.com>
2023-03-11 20:56:35 +0100

    Made BackToTop component functional

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -1,28 +1,15 @@
-import { useEffect } from "react";
 import { FiArrowUp } from "react-icons/fi";
 const BackToTop = () => {
-  useEffect(() => {
-    const scooty = document.getElementById("take-me-up");
-    const scrollFunction = () => {
-      if (
-        (document.body.scrollTop || document.documentElement.scrollTop) > 20
-      ) {
-        scooty.style.display = "block";
-      } else {
-        scooty.style.display = "none";
-      }
-    };
-    // scrollFunction();
-    // console.log(scooty);
-  }, []);
   return (
     <div
-      id="take-me-up "
-      className="fixed bottom-3 right-[1px] w-10 h-10 bg-blue-100 rounded-full flex justify-center items-center cursor-pointer z-[10000]"
+      id="take-me-up"
+      className={`fixed bottom-3 right-[5px] w-10 h-10 bg-blue-100 rounded-full ${
+        document.documentElement.scrollTop > 100 ? "flex md:hidden" : "hidden"
+      } justify-center items-center cursor-pointer z-[10000]`}
     >
       <FiArrowUp
         onClick={() => {
-          window.moveTo(0, 0);
+          document.documentElement.scrollTop = 0;
         }}
       />
     </div>
