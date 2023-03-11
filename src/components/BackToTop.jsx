653a6789906f37e8b149cdee01e299a80aff1679
Abdullah Ajayi <hafizajayi2308@gmail.com>
2023-03-11 23:23:10 +0100

    Updated styling for BackToTop component

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -3,8 +3,8 @@ const BackToTop = () => {
   return (
     <div
       id="take-me-up"
-      className={`fixed bottom-3 right-[5px] w-10 h-10 bg-blue-100 rounded-full ${
-        document.documentElement.scrollTop > 100 ? "flex md:hidden" : "hidden"
+      className={`fixed bottom-3 right-[5px] md:right-[50px] w-10 h-10 bg-blue-100 rounded-full ${
+        document.documentElement.scrollTop > 200 ? "flex" : "hidden"
       } justify-center items-center cursor-pointer z-[10000]`}
     >
       <FiArrowUp
