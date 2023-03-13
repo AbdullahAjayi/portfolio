5ca4355c710da8cb902a4f14bff1f03b9f915ede
Abdullah Ajayi <hafizajayi2308@gmail.com>
2023-03-13 15:28:38 +0100

    Updated BgSvg

--- src/components/BgSvg.jsx
+++ src/components/BgSvg.jsx
@@ -2,7 +2,7 @@ import bgSvg from "../asset/svg/Jet1.svg";
 function BgSvg() {
   return (
     <object
-      className="fixed top-1/2 left-1/2 opacity-20 -translate-x-1/2 -translate-y-1/2"
+      className="fixed top-1/2 left-1/2 opacity-[.12] z-[-10000] -translate-x-1/2 -translate-y-1/2"
       data={bgSvg}
       type="image/svg+xml"
     ></object>
