1068951490c3ec986f537f9cb21c774a0faeccb4
Abdullah Ajayi <hafizajayi2308@gmail.com>
2023-03-13 01:59:30 +0100

    Added functionality to email box

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,5 @@
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import { FiArrowRight } from "react-icons/fi";
 import { globalStyles } from "../styles/globalStyles";
 
@@ -8,6 +10,59 @@ const styles = {
 };
 
 const Contact = () => {
+  const [loading, setLoading] = useState("shoot");
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  // template_8d3jq4u
+  // service_budamta
+  // 6-R2BF5L9TNq91ZNS
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading("Shooting");
+    emailjs
+      .send(
+        "service_budamta",
+        "template_8d3jq4u",
+        {
+          from_name: form.name,
+          to_name: "Abdullah",
+          from_email: form.email,
+          to_email: "hafizajayi2308@gmail.com",
+          message: form.message,
+        },
+        "6-R2BF5L9TNq91ZNS"
+      )
+      .then(
+        () => {
+          alert("Thank you! I will get back to you as soon as possible");
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+
+          setLoading("Shoot");
+        },
+        (error) => {
+          alert(
+            "Sorry, something went wrong. Pls email us at hafizajayi2308@gmail.com"
+          );
+        }
+      );
+  };
   return (
     <section
       id="contact"
@@ -25,13 +80,17 @@ const Contact = () => {
         Send me a mail.
       </p>
       <form
-        onSubmit={() => {}}
+        onSubmit={handleSubmit}
         className="box animate-on-scroll mt-10 w-full max-w-[500px] flex flex-col gap-5"
       >
-        <div className="flex flex-col md:flex-row  gap-3">
+        <div className="flex flex-col gap-3">
           <input
             className={styles.input}
             type="text"
+            name="name"
+            id="name"
+            value={form.name}
+            onChange={handleChange}
             placeholder="Enter your name"
           />
           <input
@@ -39,16 +98,22 @@ const Contact = () => {
             type="email"
             name="email"
             id="email"
+            value={form.email}
+            onChange={handleChange}
             placeholder="Enter your email"
             required
           />
         </div>
         <div className="w-full">
           <textarea
-            required
             className={`${styles.input} h-[150px] resize-none`}
             type="text"
+            name="message"
+            id="message"
+            value={form.message}
+            onChange={handleChange}
             placeholder="Your message"
+            required
           />
         </div>
         <div className="flex flex-1 justify-center items-center cursor-pointer">
@@ -56,7 +121,7 @@ const Contact = () => {
             <input
               className="text-sm font-semibold uppercase"
               type="button"
-              value="Shoot"
+              value={loading}
             />
             <FiArrowRight className="arrow text-xl hover:translate-x-1" />
           </button>
