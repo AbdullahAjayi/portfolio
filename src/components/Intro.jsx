9bddf3fd52017cb9c89f764968ba114fd27b3896
AbdullahAjayi <hafizajayi2308@gmail.com>
2025-06-09 12:42:43 +0100

    updated intro section

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,12 +1,11 @@
-import ProfilePic from "../asset/images/ProfilePic.png";
-import { globalStyles } from "../styles/globalStyles";
+import ProfilePic from "../asset/images/ProfilePic.png"
+import { globalStyles } from "../styles/globalStyles"
 
 const styles = {
-  flexContainer:
-    "flex flex-col-reverse items-center md:flex-row justify-between",
+  flexContainer: "flex flex-col-reverse items-center md:flex-row justify-between",
   flexLeft: "md:w-500 text-center md:text-left",
   flexRight: "w-300 flex justify-center",
-};
+}
 
 const Intro = () => {
   return (
@@ -17,20 +16,15 @@ const Intro = () => {
             <h1 className={`${globalStyles.subheading} font-semibold`}>
               Hi there!, I am Abdullah, a
             </h1>
-            <h1 className={`${globalStyles.heading}  mt-2`}>
-              Frontend Developer.
-            </h1>
+            <h1 className={`${globalStyles.heading}  mt-2`}>Frontend Developer.</h1>
           </div>
           <p className={`${globalStyles.sectionParagraph} text-lg md:text-xl`}>
-            I like to craft cutting-edge user interfaces with intuitively
-            implemented UX.
+            I craft cutting-edge user interfaces with intuitively implemented UX.
           </p>
           <p className={`${globalStyles.sectionParagraph} text-lg md:text-xl`}>
             I'm passionate about solving problems and bringing ideas to life.
           </p>
-          <div
-            className={`${globalStyles.btn} px-10 w-full md:w-fit p-3 mt-8 text-lg`}
-          >
+          <div className={`${globalStyles.btn} px-10 w-full md:w-fit p-3 mt-8 text-lg`}>
             <a href="#contact">Let's work together</a>
           </div>
         </div>
@@ -46,7 +40,7 @@ const Intro = () => {
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Intro;
+export default Intro
